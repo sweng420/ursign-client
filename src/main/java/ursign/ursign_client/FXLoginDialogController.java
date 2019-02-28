40ea9dfafadb53b3362bbbeee4d0db422f4eeb06
Iyra Singh Gaura Bhandal <isgb500@its.york.ac.uk>
2019-02-28 14:49:33 +0000

    Started implementing with JavaFX

---
+++ src/main/java/ursign/ursign_client/FXLoginDialogController.java
@@ -0,0 +1,14 @@
+package ursign.ursign_client;
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+ 
+public class FXLoginDialogController {
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+
+}
