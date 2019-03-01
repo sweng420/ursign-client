91afde497d0265b556ae8e396bb9aafbc83228ed
Iyra <iyra@nowhere>
2019-03-01 11:22:57 +0000

    FXML Login dialogue shows up now

--- src/main/java/ursign/ursign_client/App.java
+++ src/main/java/ursign/ursign_client/App.java
@@ -20,16 +20,36 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
-public class App 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-    	User u = new User();
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("FXLoginDialog.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+	public static void main(String[] args) {
+		//launch(args);
+		
+		User u = new User();
 
     	try {
-			LoginDialog dialog = new LoginDialog(u);
+			/*LoginDialog dialog = new LoginDialog(u);
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
+			dialog.setVisible(true);*/
+    		launch(args);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -39,11 +59,18 @@ public class App
     		return;
     	}
     	System.out.print("Welcome!");
-    	/* put here what you want to do when the login worked */
+    	
     	
     	CollectionParser parser = new CollectionParser();
     	parser.parse("colours2.xml");
-    System.out.println(parser.getPresentation());
-        
-    }
+    	 System.out.println(parser.getPresentation());
+	}
+	
+    /*public static void main( String[] args )
+    {
+    	
+    
+    	FXLoginDialog ld = new FXLoginDialog();
+       
+    }*/
 }
--- src/main/java/ursign/ursign_client/FXLoginDialogController.java
+++ src/main/java/ursign/ursign_client/FXLoginDialogController.java
@@ -2,13 +2,20 @@ package ursign.ursign_client;
  
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
  
 public class FXLoginDialogController {
+	@FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
     @FXML private Text actiontarget;
     
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Sign in button pressed");
+        
+        String uname = usernameField.getText();
+        String password = passwordField.getText();
     }
 
 }
